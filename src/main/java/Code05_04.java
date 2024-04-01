ccbf7f77d6fa9de47a27d0ede81395e093be0a9d
data8320-36 <arsion0409@gmail.com>
2024-04-01 14:39:35 +0900

    수정

---
+++ src/main/java/Code05_04.java
@@ -0,0 +1,15 @@
+public class Code05_04 {
+    public static void main(String[] args) {
+        int num = 200;
+
+        if (num < 100) {
+            System.out.print("100보다 작군요");
+            System.out.print("여기는 참입니다");
+        } else {
+            System.out.print("100보다 크군요");
+            System.out.print("여기는 거짓입니다");
+        }
+
+        System.out.print("프로그램 끝!");
+    }
+}
