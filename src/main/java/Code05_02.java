8df71ddafdf6fdd1ba13c553e77b8703020f9901
data8320-36 <arsion0409@gmail.com>
2024-04-01 14:02:42 +0900

    수정

--- src/main/java/Code05_02.java
+++ src/main/java/Code05_02.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 public class Code05_02 {
     public static void main(String[] args) {
-        int num = 99;
 
-        if (num < 100) {
+        Scanner s = new Scanner(System.in);
+
+        int a;
+        a = s.nextInt();
+
+        if (a < 100) {
             System.out.print("100보다 ");
             System.out.print("작습니다.");
         }
